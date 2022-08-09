bbb6368efa19ccc70d6874f1765be93a495f7e31
tcgename <110209688+tcgename@users.noreply.github.com>
2022-08-09 16:25:18 +0800

    list

--- practical 4/list_exercises.py
+++ practical 4/list_exercises.py
@@ -7,10 +7,10 @@ for i in range(1,6):
     number = numbers[i - 1]
     total += number[i-1]
 average = total/5
-print(numbers[0])
-print(numbers[4])
-print(min(numbers))
-print(max(numbers))
-print(average)
+print(f"the first number is {numbers[0]}")
+print(f"the last number is {numbers[4]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average of the numbers is {average}")
 
 
