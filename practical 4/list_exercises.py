2e0b3120e8095d80573d395e0a497b4ecdbef470
tcgename <110209688+tcgename@users.noreply.github.com>
2022-08-09 16:22:16 +0800

    list

---
+++ practical 4/list_exercises.py
@@ -0,0 +1,15 @@
+numbers = []
+for i in range(1,6):
+    number = input("Number: ")
+    numbers.append(number)
+total = 0
+for i in range(1,6):
+    number = numbers[i - 1]
+    total += number[i-1]
+average = total/5
+print(numbers[0])
+print(numbers[4])
+print(min(numbers))
+print(max(numbers))
+print(average)
+
